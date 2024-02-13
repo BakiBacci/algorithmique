14f0ce32079adadb7507431151791850b5158a1f
BakiBacci <isaakibat@gmail.com>
2024-02-13 16:57:39 +0100

    JS

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// commentaire en JS 
+/*
+commentaire sur les ligne
+
+*/
+// pour afficher des informations dans la console de débugage du navigateur 
+console.log("hello, world")
+
+/* -------les variables-------*/
+// déclaration d'une variable
+let uneVariable;
+
+// affectation d'une variable
+uneVariable = 'je suis une varaible'
+
+// les 2 se font généralement en même temps 
+// les variables sont sensibles à la casse
+
+let helloWorld = 'Hello, World!';
+
+console.log(helloWorld);
+// Les différents type de variables 
+let messages = 'un message'
+let number = 20
+let decimal = 1.5
+let boolean = true;
+let boolean2 = false
+
+// les opérations arthmétiques (+,-,*,/)
+// Déclarer 3 variables sum, number1, number2
+//affecter 10 à number1
+// affecter 20 à number2
+//affecter à sum l'opération de multiplication des deux variables précédentes 
+
+let sum 
+let number1 = 10
+let number2 = 20
+sum = number1*number2
+console.log(sum)
+/*-----la concatenation----*/
+
+let Name = 'prénom'
+let SurName = "nom"
+
+console.log("je suis" + " "+  Name + " " + SurName)
+
+// Déclarer une variable city, afficher le message j'adore vivre à city
+let city = 'Paris'
+console.log("j'adore vivre à et me balader à" + "" + city) 
+city = 'Paris'
+city = `Paris`
+console.log(`j'adore vire à ${city}`)
+// J'adore vivre à <city> et je m'appelle <Name>
+console.log(`j'adore vivre à ${city} et je m'appelle ${Name} ${SurName}`)
+
